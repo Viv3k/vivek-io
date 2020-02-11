34763ae607a8a7c55996fae8fd966c4811aaad15
Vivek <vivek.darsanapu@inmobi.com>
2020-02-11 16:04:34 +0530

    Updates

---
+++ precache-manifest.d43e6f86353f80671bc29d7d9b8cb5d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b77bce752d5f96efc91f3c5e039a79e2",
+    "url": "/viv3k.github.io/index.html"
+  },
+  {
+    "revision": "bbbfb3c415c005ebe33c",
+    "url": "/viv3k.github.io/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5f58f525a8c46557eb87",
+    "url": "/viv3k.github.io/static/js/2.e3d9c8ad.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/viv3k.github.io/static/js/2.e3d9c8ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbbfb3c415c005ebe33c",
+    "url": "/viv3k.github.io/static/js/main.86569f39.chunk.js"
+  },
+  {
+    "revision": "6a7d66fa1fd8ba2738e4",
+    "url": "/viv3k.github.io/static/js/runtime-main.2b492693.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/viv3k.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vivek-io/precache-manifest.59f61d0f7662e3694d9098ade71a3646.js"
+  "/viv3k.github.io/precache-manifest.d43e6f86353f80671bc29d7d9b8cb5d6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/vivek-io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/viv3k.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
